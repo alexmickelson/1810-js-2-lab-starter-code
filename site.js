1b6518d1c7b1d420a40f03a4eb87341e86e2b019
Alex Mickelson <alexmickelson96@gmail.com>
2024-12-19 10:11:20 -0700

    updates

--- site.js
+++ site.js
@@ -1,10 +1,16 @@
-const buildBody = () => {
+const buildHeader = () => {
   return "<h1>Hello world</h1>";
 };
 
-function buildHeader() {
-  return "<p>this is the header</p>";
+function buildMain() {
+  return "<p>this is the body</p>";
 }
 
-document.write(buildBody());
-document.write(buildHeader());
+const buildSiteLayout = () => {
+  const header = buildHeader();
+  const body = buildMain();
+  return `${header} ${body}`;
+};
+
+const siteHTML = buildSiteLayout();
+document.write(siteHTML);
